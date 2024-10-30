f8192ee7923145c4b046c323473d24387bffed22
Sindhu Bharadwaj <148423220+McKenzieaa@users.noreply.github.com>
2024-10-30 12:11:16 +0530

    css fix

--- streamlit_dashboard/1_🏠Home.py
+++ streamlit_dashboard/1_🏠Home.py
@@ -9,7 +9,7 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Load CSS
-local_css("styles.css")
+local_css("McKenzieaa/mck_cfo/streamlit_dashboard/assets/icons/style.css")
 
 # Header Section
 st.markdown("""
