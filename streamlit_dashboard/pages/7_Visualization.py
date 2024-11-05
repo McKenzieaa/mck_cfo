1238bada75a815435f7c21eadbacdb613f540d5d
Sindhu Bharadwaj <148423220+McKenzieaa@users.noreply.github.com>
2024-11-05 12:16:21 +0530

    fix

--- streamlit_dashboard/pages/7_Visualization.py
+++ streamlit_dashboard/pages/7_Visualization.py
@@ -1,15 +1,19 @@
+import streamlit as st
 import pandas as pd
 import pygwalker as pyg
 import numpy as np
 
+# Load the data
 path_public_comps = r'streamlit_dashboard/data/Public Listed Companies US.xlsx'
-
 df = pd.read_excel(path_public_comps, sheet_name="FY 2023")
+
+# Data processing
 df['Enterprise Value (in $)'] = pd.to_numeric(df['Enterprise Value (in $)'], errors='coerce')
 df['Revenue (in $)'] = pd.to_numeric(df['Revenue (in $)'], errors='coerce').round(1)
 df['EBITDA (in $)'] = pd.to_numeric(df['EBITDA (in $)'], errors='coerce').round(1)
 df['EV/Revenue'] = df['Enterprise Value (in $)'] / df['Revenue (in $)']
 df['EV/EBITDA'] = df['Enterprise Value (in $)'] / df['EBITDA (in $)']
+
 columns_to_display = ['Name', 'Country', 'Industry', 'EV/Revenue', 'EV/EBITDA', 'Business Description']
 df = df[columns_to_display]
 df.replace("-", np.nan, inplace=True)
@@ -19,4 +23,8 @@ df = df.dropna(subset=['Country', 'Industry', 'EV/Revenue', 'EV/EBITDA'])
 df['EV/Revenue'] = df['EV/Revenue'].apply(lambda x: f"{x:.1f}x")
 df['EV/EBITDA'] = df['EV/EBITDA'].apply(lambda x: f"{x:.1f}x")
 
+# Display in Streamlit
+st.title("Public Listed Companies in the US - FY 2023")
+
+# Use Pygwalker for interactive visualizations
 pyg.walk(df, appearance='light')
