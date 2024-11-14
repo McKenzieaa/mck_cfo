5fc826794749367b60571adf1b2ec53939dbf57b
sindhusb03 <sindhusb03@hotmail.com>
2024-11-14 19:50:56 +0530

    fix

--- streamlit_dashboard/pages/4_US Indicators.py
+++ streamlit_dashboard/pages/4_US Indicators.py
@@ -539,32 +539,43 @@ def plot_external_driver(selected_indicators):
     return fig
 
 def plot_cpi_ppi(selected_series_id):
+    """
+    Plot CPI and PPI data on a single chart for comparison.
+    """
     fig = go.Figure()
     cpi_data = fetch_cpi_data(selected_series_id, df_cleaned)
-    if not cpi_data.empty: fig.add_trace(
+    if not cpi_data.empty:
+        fig.add_trace(
             go.Scatter(
-                x=cpi_data['date'], y=cpi_data['value'], mode='lines', name='CPI by Industry', line=dict(color='#032649')
-            )
-    ) # Add CPI-All Items line
-    if not all_items_data.empty: fig.add_trace(
+                x=cpi_data['date'],y=cpi_data['value'],mode='lines',name='CPI by Industry',line=dict(color='#032649'))
+        )
+    else:
+        st.warning(f"No data available for the selected CPI series: {selected_series_id}")
+
+    if not all_items_data.empty:
+        fig.add_trace(
             go.Scatter(
-                x=all_items_data['Month & Year'], y=all_items_data['Value'], mode='lines', name='CPI-US', line=dict(color='#EB8928', dash='solid')
-            )
-    ) # Add PPI line
+                x=all_items_data['Month & Year'], y=all_items_data['Value'], mode='lines', name='CPI-US', line=dict(color='#EB8928', dash='solid'))
+        )
+    else:
+        st.warning("No CPI-US All Items data available to display.")
+
     if not df_ppi_unpivoted.empty:
         df_ppi_aggregated = df_ppi_unpivoted.groupby('Month & Year', as_index=False).agg({'Value': 'mean'})
-    fig.add_trace(
+        fig.add_trace(
             go.Scatter(
-                x=df_ppi_aggregated['Month & Year'], y=df_ppi_aggregated['Value'], mode='lines', name='PPI-US', line=dict(color='#595959')
-            )
+                x=df_ppi_aggregated['Month & Year'],y=df_ppi_aggregated['Value'],mode='lines',name='PPI-US',line=dict(color='#595959'))
         )
+    else:
+        st.warning("No PPI data available to display.")
+
     fig.update_layout(
-        title='CPI and PPI Comparison',
+        title='CPI and PPI ',
         xaxis=dict(showgrid=True, showticklabels=True),
         yaxis=dict(title='Value'),
         hovermode='x unified'
     )
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)#, key=key)
     return fig
 
 def plot_gdp_and_industry(selected_industry=None):
