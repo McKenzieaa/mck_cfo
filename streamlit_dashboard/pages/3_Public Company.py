45dc0bb5a9b23b78514e6a33057c440787c736b3
sindhusb03 <sindhusb03@hotmail.com>
2024-11-28 08:48:34 +0530

    fix

--- streamlit_dashboard/pages/3_Public Company.py
+++ streamlit_dashboard/pages/3_Public Company.py
@@ -104,7 +104,7 @@ if selected_industries and selected_locations:
 
         # Create the EV/Revenue chart with data labels
         fig1 = px.bar(avg_data, x='Company', y='EV/Revenue', title="EV/Revenue", text='EV/Revenue')
-        fig1.update_traces(marker_color=color_ev_revenue, texttemplate='%{text:.1f}'+'x', textposition='outside')
+        fig1.update_traces(marker_color=color_ev_revenue, texttemplate='%{text:.1f}'+'x', textposition='outside',textfont=dict(size=10))
         fig1.update_layout(yaxis_title="EV/Revenue", xaxis_title=" ",bargap=0.1,bargroupgap=0.1,yaxis=dict(showgrid=False),xaxis=dict(tickangle=0,automargin=True))
 
         # Display the EV/Revenue chart
@@ -112,7 +112,7 @@ if selected_industries and selected_locations:
 
         # Create the EV/EBITDA chart with data labels
         fig2 = px.bar(avg_data, x='Company', y='EV/EBITDA', title="EV/EBITDA", text='EV/EBITDA')
-        fig2.update_traces(marker_color=color_ev_ebitda,texttemplate='%{text:.1f}'+'x', textposition='outside')
+        fig2.update_traces(marker_color=color_ev_ebitda,texttemplate='%{text:.1f}'+'x', textposition='outside',textfont=dict(size=10))
         fig2.update_layout(yaxis_title="EV/EBITDA", xaxis_title=" ",bargap=0.1,bargroupgap=0.1,yaxis=dict(showgrid=False),xaxis=dict(tickangle=0,automargin=True))
 
         # Display the EV/EBITDA chart
