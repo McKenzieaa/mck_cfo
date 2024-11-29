a1051cfd72dc8606b62691c2ff0b85e0cbec579f
sindhusb03 <sindhusb03@hotmail.com>
2024-11-29 11:07:28 +0530

    fix

--- streamlit_dashboard/pages/8_Pitch Book.py
+++ streamlit_dashboard/pages/8_Pitch Book.py
@@ -1113,6 +1113,7 @@ with st.expander("Public Comps"):
 
         if not selected_data.empty:
             avg_data = selected_data.groupby('Company')[['EV/Revenue', 'EV/EBITDA']].mean().reset_index()
+            avg_data['Company'] = avg_data['Company'].apply(lambda x: '<br>'.join([x[i:i+10] for i in range(0, len(x), 10)]))
 
             color_ev_revenue = "#032649"  # Default Plotly blue
             color_ev_ebitda = "#032649"   # Default Plotly red
