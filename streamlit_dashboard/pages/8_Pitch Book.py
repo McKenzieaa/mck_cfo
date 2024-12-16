af2e2584bde8c6f5291976e841ecdf49ad3dad6e
sindhusb03 <sindhusb03@hotmail.com>
2024-12-16 16:59:28 +0530

    fix back

--- streamlit_dashboard/pages/8_Pitch Book.py
+++ streamlit_dashboard/pages/8_Pitch Book.py
@@ -133,7 +133,7 @@ def add_table_to_slide(slide, df, left, top, width, height, font_size=Pt(10), he
             cell.margin_left = Inches(0.05)
             cell.margin_right = Inches(0.05)
 
-def export_all_to_pptx(labour_fig_us, external_fig, gdp_fig_us, cpi_ppi_fig_us, fig1_precedent, fig2_precedent, fig1_public, fig2_public, labour_fig, gdp_fig,category_charts):
+def export_all_to_pptx(labour_fig_us, external_fig, gdp_fig_us, cpi_ppi_fig_us, fig1_precedent, fig2_precedent, fig1_public, fig2_public, labour_fig, gdp_fig):
     # Load the custom template
     template_path = os.path.join(os.getcwd(), "streamlit_dashboard", "data", "main_template_pitch.pptx")
     ppt = Presentation(template_path)  # Load the template
@@ -149,12 +149,6 @@ def export_all_to_pptx(labour_fig_us, external_fig, gdp_fig_us, cpi_ppi_fig_us, 
     update_figure_slide(ppt, "CPI and PPI Comparison", cpi_ppi_fig_us, slide_number=5, width=5, height=2.50, left=5.10, top=1.3)
     update_figure_slide(ppt, f"Labour force Statitics {state_name}", labour_fig, slide_number=4, width=5, height=2.50, left=0.08, top=1.3)
     update_figure_slide(ppt, f"GDP - {state_name} ", gdp_fig, slide_number=4, width=5, height=2.50, left=0.08, top=4.4)
-    update_figure_slide(ppt, "IBIS", category_charts, slide_number=8, positions = [
-        {"left": Inches(1), "top": Inches(1), "width": Inches(6), "height": Inches(3)},  # Position for Chart 1
-        {"left": Inches(1), "top": Inches(4.5), "width": Inches(6), "height": Inches(3)},  # Position for Chart 2
-        {"left": Inches(7.5), "top": Inches(1), "width": Inches(6), "height": Inches(3)},  # Position for Chart 3
-        {"left": Inches(7.5), "top": Inches(4.5), "width": Inches(6), "height": Inches(3)},  # Position for Chart 4
-    ])
 
     # Add Benchmarking Tables to Slide
     slide = ppt.slides[9] 
@@ -197,10 +191,10 @@ df_unemp['month'] = pd.to_numeric(time_split_unemp[1], errors='coerce').astype('
 
 # Population Data
 df_pop = pd.read_csv(url_pop)
-df_pop = df_pop.rename(columns={'DATE': 'Date', 'POPTHM': 'population'})
-df_pop['Date'] = pd.to_datetime(df_pop['Date'])
-df_pop['year'] = df_pop['Date'].dt.year
-df_pop['month'] = df_pop['Date'].dt.month
+df_pop = df_pop.rename(columns={'DATE': 'date', 'POPTHM': 'population'})
+df_pop['date'] = pd.to_datetime(df_pop['date'])
+df_pop['year'] = df_pop['date'].dt.year
+df_pop['month'] = df_pop['date'].dt.month
 
 # External Driver Data
 external_driver_path = r"streamlit_dashboard/data/business_enviornmental_profiles.csv"
@@ -1489,8 +1483,7 @@ with st.expander("IBIS"):
                     xanchor='left', 
                     yanchor='top'
                 ),
-                yaxis=dict(showgrid=False),
-                margin=dict(l=50, r=50, t=50,b=50),height=400,width=600
+                yaxis=dict(showgrid=False)
             )
 
             category_charts.append(fig)
