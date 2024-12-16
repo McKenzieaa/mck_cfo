3f942c529e86e7d1ef848bdbad6bc807fa42f59d
sindhusb03 <sindhusb03@hotmail.com>
2024-12-16 14:39:04 +0530

    fix

--- streamlit_dashboard/pages/10_IBIS.py
+++ streamlit_dashboard/pages/10_IBIS.py
@@ -91,8 +91,8 @@ def create_category_charts(df):
                 name='Change (%)',
                 mode='lines+markers',
                 line=dict(color=line_color),
-                text=[f"{change:.2f}%" if i == len(category_data) - 1 else "" for i, change in enumerate(category_data['Change'])],  # Show text only for the last value
-                textposition="top center"  # Place text above the last marker
+                text=[f"{change:.1f}%" if i == len(category_data) - 1 else "" for i, change in enumerate(category_data['Change'])],  # Show text only for the last value
+                textposition="inside"  # Place text above the last marker
             ),
             secondary_y=True
         )
