cd750efb2d0d7768069e923f24321ef9f817f55d
sindhusb03 <sindhusb03@hotmail.com>
2024-12-18 17:20:42 +0530

    fix

--- streamlit_dashboard/pages/8_Pitch Book.py
+++ streamlit_dashboard/pages/8_Pitch Book.py
@@ -136,7 +136,7 @@ def add_table_to_slide(slide, df, left, top, width, height, font_size=Pt(10), he
 def export_all_to_pptx(
     labour_fig_us, external_fig, gdp_fig_us, cpi_ppi_fig_us,
     fig1_precedent, fig2_precedent, fig1_public, fig2_public,
-    labour_fig, gdp_fig, income_statement_df, income_fig, balance_fig,
+    labour_fig, gdp_fig, income_statement_df, 
     balance_sheet_df, state_name
 ):
     # Load the custom template
@@ -154,8 +154,8 @@ def export_all_to_pptx(
     update_figure_slide(ppt, "CPI and PPI Comparison", cpi_ppi_fig_us, slide_number=5, width=5, height=2.50, left=5.10, top=1.3)
     update_figure_slide(ppt, f"Labour force Statistics {state_name}", labour_fig, slide_number=4, width=5, height=2.50, left=0.08, top=1.3)
     update_figure_slide(ppt, f"GDP - {state_name}", gdp_fig, slide_number=4, width=5, height=2.50, left=0.08, top=4.4)
-    update_figure_slide(ppt, "RMA-Income Statement", income_fig, slide_number=10, width=9, height=3, left=0.45, top=0.90)
-    update_figure_slide(ppt, "RMA-Balance Sheet", balance_fig, slide_number=10, width=9, height=3, left=0.45, top=3.60)
+    # update_figure_slide(ppt, "RMA-Income Statement", income_fig, slide_number=10, width=9, height=3, left=0.45, top=0.90)
+    # update_figure_slide(ppt, "RMA-Balance Sheet", balance_fig, slide_number=10, width=9, height=3, left=0.45, top=3.60)
 
     # Add Benchmarking Tables to Slide
     slide = ppt.slides[9]
@@ -1548,11 +1548,11 @@ with st.expander("Benchmarking"):
 
         st.write("Income Statement")
         st.dataframe(income_statement_df.fillna(np.nan), hide_index=True, use_container_width=True)
-        st.plotly_chart(income_fig, use_container_width=True)
+        # st.plotly_chart(income_fig, use_container_width=True)
 
         st.write("Balance Sheet")
         st.dataframe(balance_sheet_df.fillna(np.nan), hide_index=True, use_container_width=True)
-        st.plotly_chart(balance_fig, use_container_width=True)
+        # st.plotly_chart(balance_fig, use_container_width=True)
 
 with st.expander("IBIS"):
     st.subheader("IBIS - Industry Report")
