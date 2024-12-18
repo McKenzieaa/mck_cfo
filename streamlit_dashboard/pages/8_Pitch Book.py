d4375fdcb366fa320331f0efbd45b49bb083a100
sindhusb03 <sindhusb03@hotmail.com>
2024-12-18 13:57:41 +0530

    fix

--- streamlit_dashboard/pages/8_Pitch Book.py
+++ streamlit_dashboard/pages/8_Pitch Book.py
@@ -154,19 +154,13 @@ def export_all_to_pptx(
     update_figure_slide(ppt, "CPI and PPI Comparison", cpi_ppi_fig_us, slide_number=5, width=5, height=2.50, left=5.10, top=1.3)
     update_figure_slide(ppt, f"Labour force Statistics {state_name}", labour_fig, slide_number=4, width=5, height=2.50, left=0.08, top=1.3)
     update_figure_slide(ppt, f"GDP - {state_name}", gdp_fig, slide_number=4, width=5, height=2.50, left=0.08, top=4.4)
-
-    # Define positions for category charts
-    positions = [
+    update_figure_slide(ppt, "IBIS Chart", chart, slide_number=7, positions = [
         {"left": Inches(1), "top": Inches(1), "width": Inches(6), "height": Inches(3)},  # Position for Chart 1
         {"left": Inches(1), "top": Inches(4.5), "width": Inches(6), "height": Inches(3)},  # Position for Chart 2
         {"left": Inches(7.5), "top": Inches(1), "width": Inches(6), "height": Inches(3)},  # Position for Chart 3
         {"left": Inches(7.5), "top": Inches(4.5), "width": Inches(6), "height": Inches(3)},  # Position for Chart 4
-    ]
-
-    # Add category charts to slide 7
-    for chart, pos in zip(category_charts, positions):
-        update_figure_slide(ppt, "IBIS Chart", chart, slide_number=7, **pos)
-
+    ])
+    
     # Add Benchmarking Tables to Slide
     slide = ppt.slides[9]
     add_table_to_slide(slide, income_statement_df, left=0.35, top=0.90, width=4.3, height=3.4, header_font_size=Pt(12))
