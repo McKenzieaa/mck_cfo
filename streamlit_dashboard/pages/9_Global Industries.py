f040395e8e4ef682a5ca88d8bde8d01f4f958194
sindhusb03 <sindhusb03@hotmail.com>
2024-12-22 10:53:03 +0530

    fix

--- streamlit_dashboard/pages/9_Global Industries.py
+++ streamlit_dashboard/pages/9_Global Industries.py
@@ -374,8 +374,6 @@ def export_chart_options(fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8, value_c
         except FileNotFoundError as e:
             st.error(f"Error: {e}")
 
-# Example of calling the function with corrected file path
-value_chain_image_path =os.path.abspath("streamlit_dashboard\data\value_chain.png")  # Use raw string or correct the slashes
-
+value_chain_image_path = r"/mount/src/mck_cfo/streamlit_dashboard/data/value_chain.png"
 # Example call to the function (ensure fig1, fig2, etc. are defined)
 export_chart_options(fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8, value_chain_image_path)
