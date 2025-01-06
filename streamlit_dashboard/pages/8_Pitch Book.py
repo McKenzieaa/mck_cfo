e058602dd960fb3d043c201e3442d3b180855d1a
sindhusb03 <sindhusb03@hotmail.com>
2025-01-06 15:55:37 +0530

    fix back

--- streamlit_dashboard/pages/8_Pitch Book.py
+++ streamlit_dashboard/pages/8_Pitch Book.py
@@ -136,7 +136,7 @@ def add_table_to_slide(slide, df, left, top, width, height, font_size=Pt(10), he
 def export_all_to_pptx(
     labour_fig_us, external_fig, gdp_fig_us, cpi_ppi_fig_us,
     fig1_precedent, fig2_precedent, fig1_public, fig2_public,
-    labour_fig, gdp_fig, income_statement_df, income_fig, balance_fig,
+    labour_fig, gdp_fig, income_statement_df, 
     balance_sheet_df, state_name, fig1_ibis, fig2_ibis, fig3_ibis, fig4_ibis
 ):
     # Load the custom template
@@ -154,8 +154,8 @@ def export_all_to_pptx(
     update_figure_slide(ppt, "CPI and PPI Comparison", cpi_ppi_fig_us, slide_number=5, width=5, height=2.50, left=5.10, top=1.3)
     update_figure_slide(ppt, f"Labour force Statistics {state_name}", labour_fig, slide_number=4, width=5, height=2.50, left=0.08, top=1.3)
     update_figure_slide(ppt, f"GDP - {state_name}", gdp_fig, slide_number=4, width=5, height=2.50, left=0.08, top=4.4)
-    update_figure_slide(ppt, "RMA-Income Statement", income_fig, slide_number=10, width=9, height=3, left=0.45, top=0.90)
-    update_figure_slide(ppt, "RMA-Balance Sheet", balance_fig, slide_number=10, width=9, height=3, left=0.45, top=3.60)
+    # update_figure_slide(ppt, "RMA-Income Statement", income_fig, slide_number=10, width=9, height=3, left=0.45, top=0.90)
+    # update_figure_slide(ppt, "RMA-Balance Sheet", balance_fig, slide_number=10, width=9, height=3, left=0.45, top=3.60)
 
     # Add IBIS charts
     update_figure_slide(ppt, "Profit - IBIS", fig1_ibis, slide_number=8, width=4.75, height=2.45, left=0.18, top=4.34)
@@ -1595,7 +1595,7 @@ if st.button("Export Charts to PowerPoint", key="export_button"):
         pptx_file = export_all_to_pptx(
             labour_fig_us, external_fig, gdp_fig_us, cpi_ppi_fig_us, 
             fig1_precedent, fig2_precedent, fig1_public, fig2_public, 
-            labour_fig, gdp_fig, income_fig, balance_fig,
+            labour_fig, gdp_fig,
             income_statement_df, balance_sheet_df, state_name,fig1_ibis, fig2_ibis, fig3_ibis, fig4_ibis
         )
 
