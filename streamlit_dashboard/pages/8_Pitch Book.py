a3d7c00b32a01fa3acf1ab4787d3c9b4c84f3424
sindhusb03 <sindhusb03@hotmail.com>
2025-01-07 13:31:33 +0530

    fix

--- streamlit_dashboard/pages/8_Pitch Book.py
+++ streamlit_dashboard/pages/8_Pitch Book.py
@@ -149,7 +149,7 @@ def export_all_to_pptx(
     update_figure_slide(ppt, "Public Comps - EV/Revenue", fig1_public, slide_number=12, width=9, height=3, left=0.45, top=0.90)
     update_figure_slide(ppt, "Public Comps - EV/EBITDA", fig2_public, slide_number=12, width=9, height=3, left=0.45, top=3.60)
     update_figure_slide(ppt, "Labour Force & Unemployment", labour_fig_us, slide_number=5, width=4.5, height=2.50, left=0.08, top=1.3)
-    update_figure_slide(ppt, "External Driver Indicators", external_fig, slide_number=7, width=5, height=3, left=5, top=1.3)
+    update_figure_slide(ppt, "External Driver Indicators", external_fig, slide_number=7, width=5, height=4, left=5, top=1.3)
     update_figure_slide(ppt, "GDP by Industry", gdp_fig_us, slide_number=5, width=4.5, height=2.50, left=0.08, top=4.4)
     update_figure_slide(ppt, "CPI and PPI Comparison", cpi_ppi_fig_us, slide_number=4, width=5, height=2.50, left=5.10, top=1.3)
     update_figure_slide(ppt, f"Labour force Statistics {state_name}", labour_fig, slide_number=4, width=4.5, height=2.50, left=0.08, top=1.3)
@@ -840,9 +840,9 @@ def plot_external_driver(selected_indicators):
         ),
         plot_bgcolor='rgba(0,0,0,0)', 
         paper_bgcolor='rgba(0,0,0,0)',
-        height=300,  # Increased height
+        height=400,  # Increased height
         width=500,  # Increased width
-        margin=dict(b=30, t=20, l=30, r=25),
+        margin=dict(b=100, t=20, l=30, r=25),
     )
 
     st.plotly_chart(fig, use_container_width=True)
