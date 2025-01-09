2f05151566619108b4cb487ac57801fe83824289
sindhusb03 <sindhusb03@hotmail.com>
2025-01-09 19:37:21 +0530

    fix trail

--- streamlit_dashboard/pages/6_ Benchmarking.py
+++ streamlit_dashboard/pages/6_ Benchmarking.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import s3fs 
+import s3fs
 from io import BytesIO
 from pptx import Presentation
 from pptx.util import Inches
@@ -13,25 +13,19 @@ storage_options = {
         'client_kwargs': {'region_name': st.secrets["aws"]["AWS_DEFAULT_REGION"]}
 }
 
-# Load the data from S3
 s3_path = "s3://documentsapi/industry_data/benchmarking.csv"
-# st.write("Loading data from:", s3_path)
 
 df = pd.read_csv(s3_path, storage_options=storage_options)
 df["Public Comps"] = df["File"] == "Public comps"
 df["RMA"] = df["File"] == "RMA"
 
-# Filter columns
-df = df[["Industry", "LineItems", "File", "ReportID"]]
+df = df[["Industry", "LineItems", "ReportID", "Public Comps", "RMA"]]
 
-# Filter data for "Income Statement" and display table with bar chart
 income_statement_df = df[df["ReportID"] == "Income Statement"]
 
 st.header("Income Statement")
-st.write("Table:")
 st.dataframe(income_statement_df)
 
-st.write("Bar Chart:")
 fig_income = px.bar(
     income_statement_df, x="LineItems", y=["Public Comps", "RMA"],
     title="Income Statement: Public Comps vs RMA",
@@ -39,14 +33,11 @@ fig_income = px.bar(
 )
 st.plotly_chart(fig_income)
 
-# Filter data for "Assets", "Liabilities & Equity" and display table with bar chart
 assets_liabilities_df = df[df["ReportID"].isin(["Assets", "Liabilities & Equity"])]
 
 st.header("Assets & Liabilities")
-st.write("Table:")
 st.dataframe(assets_liabilities_df)
 
-st.write("Bar Chart:")
 fig_assets_liabilities = px.bar(
     assets_liabilities_df, x="LineItems", y=["Public Comps", "RMA"],
     color="ReportID",
